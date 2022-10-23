13714e6031289775dc4455fe01a682ecee020b6e
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-24 03:00:07 +0600

    redux

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { Provider } from 'react-redux';
+import store from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
