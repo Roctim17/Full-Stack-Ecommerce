eaf33979e351f02c0a2a5f5fdf91545122bd9fa5
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-28 04:51:53 +0600

    signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -6,8 +6,10 @@ import '../Style/Login.css'
 
 const Signup = () => {
 
+    const [username,setUsername]= useState('')
     const [email,setEmail]= useState('')
     const [password, setPassword]= useState('')
+    const [file, setFile]= useState(null)
 
     return (
         <Helmet title='Signup'>
@@ -18,15 +20,23 @@ const Signup = () => {
                             <h3 className='fw-bold mb-4'>Signup</h3>
                             <Form className='auth_form'>
                                 <FormGroup className='form_group '>
+                                    <input type="text" placeholder='Email your email'
+                                    value={username} onChange={e=> setUsername(e.target.value)} />
+                                </FormGroup>
+                                <FormGroup className='form_group '>
                                     <input type="email" placeholder='Email your email'
                                     value={email} onChange={e=> setEmail(e.target.value)} />
                                 </FormGroup>
                                 <FormGroup className='form_group '>
                                     <input type="password" placeholder='Email your password' 
                                     value={password} onChange={e=> setPassword(e.target.value)} />
-                                    <button type='submit' className="buy_btn auth_btn">Signup</button>
-                                    <p>Already have an account? <Link to='/login'>Login</Link></p>
                                 </FormGroup>
+                                <FormGroup className='form_group '>
+                                    <input type="file" 
+                                   onChange={e=> setFile(e.target.files)} />
+                                </FormGroup>
+                                    <button type='submit' className="buy_btn auth_btn">Create an Account</button>
+                                    <p>Already have an account? <Link to='/login'>Login</Link></p>
                             </Form>
                         </Col>
                     </Row>
