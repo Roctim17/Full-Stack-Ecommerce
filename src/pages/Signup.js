2d6f31de3500b4ae55e3eb752a5bccc1c2df3793
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-28 18:10:21 +0600

    firebase

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,30 +1,55 @@
-import { async } from '@firebase/util';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+// import { async } from '@firebase/util';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import React, { useState } from 'react';
+import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
 import { Col, Container, Form, FormGroup, Row } from 'reactstrap';
 import Helmet from '../components/Helmet';
-import { auth } from '../firebase.init';
+import { auth, db, storage } from '../firebase.init';
 import '../Style/Login.css'
 
 const Signup = () => {
 
-    const [username,setUsername]= useState('')
-    const [email,setEmail]= useState('')
-    const [password, setPassword]= useState('')
-    const [file, setFile]= useState(null)
-    const [loading,setLoading]=useState(false)
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [file, setFile] = useState(null)
+    const [loading, setLoading] = useState(false)
 
-    const signup = async(e)=>{
+    const signup = async (e) => {
         e.preventDefault()
         setLoading(true)
-        try{
-const userCresential = await createUserWithEmailAndPassword(auth,email,password)
-const user = userCresential.user
-        }catch (error){
+        try {
+            const userCresential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCresential.user
+            const storageRef = ref(storage, `images/${Date.now() + username}`);
+            const uploadTask = uploadBytesResumable(storageRef, file)
+            uploadTask.on((error) => {
+                toast.error(error.message)
+            }, () => {
+                getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+                    // update user profile
+                    await updateProfile(user, {
+                        displayName: username,
+                        photoURL: downloadURL
+                    });
+                    await setDoc(doc(db,"user",user.uid),{
+                        uid:user.uid,
+                        displayName:username,
+                        email,
+                        photoURL:downloadURL,
+                    })
 
+                });
+            }
+            );
+
+        } catch (error) {
+            toast.error('something wrong')
         }
-    }
+    };
 
     return (
         <Helmet title='Signup'>
@@ -33,25 +58,26 @@ const user = userCresential.user
                     <Row>
                         <Col lg='6' className='m-auto text-center'>
                             <h3 className='fw-bold mb-4'>Signup</h3>
-                            <Form className='auth_form'>
+                            <Form className='auth_form'
+                                onClick={signup}>
                                 <FormGroup className='form_group '>
                                     <input type="text" placeholder='Email your email'
-                                    value={username} onChange={e=> setUsername(e.target.value)} />
+                                        value={username} onChange={e => setUsername(e.target.value)} />
                                 </FormGroup>
                                 <FormGroup className='form_group '>
                                     <input type="email" placeholder='Email your email'
-                                    value={email} onChange={e=> setEmail(e.target.value)} />
+                                        value={email} onChange={e => setEmail(e.target.value)} />
                                 </FormGroup>
                                 <FormGroup className='form_group '>
-                                    <input type="password" placeholder='Email your password' 
-                                    value={password} onChange={e=> setPassword(e.target.value)} />
+                                    <input type="password" placeholder='Email your password'
+                                        value={password} onChange={e => setPassword(e.target.value)} />
                                 </FormGroup>
                                 <FormGroup className='form_group '>
-                                    <input type="file" 
-                                   onChange={e=> setFile(e.target.files)} />
+                                    <input type="file"
+                                        onChange={e => setFile(e.target.files)} />
                                 </FormGroup>
-                                    <button type='submit' className="buy_btn auth_btn">Create an Account</button>
-                                    <p>Already have an account? <Link to='/login'>Login</Link></p>
+                                <button type='submit' className="buy_btn auth_btn">Create an Account</button>
+                                <p>Already have an account? <Link to='/login'>Login</Link></p>
                             </Form>
                         </Col>
                     </Row>
